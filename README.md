cf3547917cbd18d30f54f7c83f91174d91cabcda
abhirajkumar-helloworld <abhirajkumar1732007@gmail.com>
2026-05-30 01:06:49 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,42 @@
+# 🎯 Number Guessing Game (Python)
+
+A simple and interactive number guessing game built using Python.  
+The player has to guess a randomly generated number within limited attempts based on difficulty level.
+
+---
+
+## 🚀 Features
+
+- 🎮 Multiple difficulty levels:
+  - Easy (1–50, 5 attempts)
+  - Medium (1–100, 7 attempts)
+  - Hard (1–500, 9 attempts)
+- 🔢 Random number generation
+- 💡 Smart hints:
+  - Too high / Too low
+  - Close guesses
+- 🔁 Replay option
+- 🛡️ Input validation (handles invalid inputs)
+
+---
+
+## 🧠 How It Works
+
+1. Choose a difficulty level
+2. The computer selects a random number
+3. Enter your guesses
+4. Get hints:
+   - "Too high" or "Too low"
+   - "Close, but high/low"
+5. Win by guessing correctly within limited attempts
+
+---
+
+## ▶️ How to Run
+
+1. Make sure Python is installed
+2. Download or clone this repository
+3. Run the file:
+
+```bash
+python main.py
---
+++ main.py
@@ -0,0 +1,55 @@
+import random
+while True:   
+    print("Its time to number guessing game")
+    print("difficulty :- ")
+    print("1. easy (1 to 50) and 5 attempts")
+    print("2. medium (1 to 100) and 7 attempts")
+    print("3. hard (1 to 500) and 9 attempts")
+    diff = input("difficulty : ")
+    if diff == "1":
+        n = 50
+        max_attempts = 5
+        print("mode easy")
+    elif diff == "2":
+        n = 100
+        max_attempts = 7
+        print ("mode medium")
+    elif diff == "3":
+        n = 500
+        max_attempts = 9
+        print ("mode hard")
+    else:
+        print ("invalid option")
+        exit()
+    sec = random.randint( 1, n )
+    attempts = 0
+    while True:
+        
+        try:    
+            guess = int(input("guess : "))
+        except:
+            print("enter a valid number")
+            continue
+        if guess == sec:
+            print("you won in ", attempts + 1, " attempts")
+            break
+        elif guess > sec:
+            if guess - sec > 3:
+                print ("too high")
+            else:
+                print ("close, but high")
+        elif guess < sec:
+            if sec - guess > 3:
+                print ("too low")
+            else :
+                print ("close, but low")
+        attempts += 1
+        print(f"you have  { max_attempts - attempts}  attempts left")
+        
+        if attempts == max_attempts:
+            print ("you lose, secret_number is ", sec)
+            break
+        print("-------------------------------")
+    again = input("play again ? (y/n)")
+    if again != "y":
+        break
